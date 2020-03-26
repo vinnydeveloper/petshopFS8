4d799819fa2b2d29e0708cac4da0d6ac45614f52
Vinicius <vinicius.developer@outlook.com>
2020-03-26 20:20:32 -0300

    Resolução do desafio, junto com exemplos de manipulação de dom

---
+++ desafio.js
@@ -0,0 +1,3 @@
+function buscarPet(nomePet){
+    //Nessa função você vai receber o nome do pet como parametro lembre-se que é uma string, seu trabalho é buscar esse pet dentro da nossa lista de pet, e dizer se existe ou não um pet com esse nome. Lembre-se de usar o console.log para dar o feedback ao usuario.
+}
--- petshop.js
+++ petshop.js
@@ -1,18 +1,4 @@
-
-// let animal = {
-//     tipo: "Gato",
-//     nome:"Elias",
-//     idade: 3,
-//     raça:undefined,
-//     porte: "Grande",
-//     sexo: "M",
-//     vacinado:true,
-//     tutor:{
-//         nome:"Vinicius",
-//         telefone:"(11)4002-8922"
-//     }
-
-// };
+let divPet = document.getElementById("pets");
 
 let listaDeAnimais = [
     {
@@ -69,10 +55,46 @@ function visualizarPets(){
     for(let i =0; i < listaDeAnimais.length; i++){
 
         let animal = listaDeAnimais[i]
-        console.log("------------------------------")
-        console.log("Nome do Animal: " + animal.nome)
-        console.log("Tipo de Animal: " + animal.tipo)
-        console.log("Sexo do Animal: " + animal.sexo)
-        console.log("Nome do tutor: " + animal.tutor.nome)
+
+        let divConteudoPet = document.createElement('div')
+        //<div></div>
+        divConteudoPet.setAttribute('class','bixinho')
+
+        let pSeparador = document.createElement('p');
+        pSeparador.textContent = "------------------------------";
+
+        let pNomeAnimal = document.createElement('p')
+        pNomeAnimal.textContent = "Nome do Animal: " + animal.nome
+       
+        let pTipoAnimal = document.createElement('p')
+        pTipoAnimal.textContent = "Tipo de Animal: " + animal.tipo
+
+        divConteudoPet.appendChild(pSeparador)
+        divConteudoPet.appendChild(pNomeAnimal)
+        divConteudoPet.appendChild(pTipoAnimal)
+
+        divPet.appendChild(divConteudoPet)
+        
+}
+}
+
+
+function buscarPet(nomePet){
+    for (let index = 0; index < listaDeAnimais.length; index++) {
+        let animal = listaDeAnimais[index];
+        
+        if(animal.nome == nomePet){
+           console.log("Sim existe esse animal cadastrado no sistema.")
+           console.log("E suas informações são essas:")
+           console.log("------------------------------")
+           console.log("Nome do Animal: " + animal.nome)
+           console.log("Tipo de Animal: " + animal.tipo)
+           console.log("Sexo do Animal: " + animal.sexo)
+           console.log("Nome do tutor: " + animal.tutor.nome)
+           return
+        }
     }
+
+    console.log('Não existe animal cadastrado!')
+   
 }
